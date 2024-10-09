5c65fcfb7ffdd6d8cf8eba28a44aa2f6fe03c0f9
Nato9-7 <re.diazs@duocuc.cl>
2024-10-09 11:39:40 -0300

    conexion a la bd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  usuarios: any[] = [];
 
-  constructor() {}
+  constructor(private usuariosService: UsuariosService) {}
 
+  ngOnInit() {
+    this.usuariosService.getUsuarios().subscribe(
+      data => {
+        console.log('Usuarios recibidos:', data);  // Para verificar si los datos llegan al componente
+        this.usuarios = data;
+      },
+      error => {
+        console.error('Error al obtener usuarios:', error);
+      }
+    );
+  }
+  
 }
