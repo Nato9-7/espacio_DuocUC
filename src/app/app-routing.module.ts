a6ccee9a69307d9e8546ba2624983df348a85fc3
Nato9-7 <re.diazs@duocuc.cl>
2024-11-04 18:27:32 -0300

    añadido la funcion para verlo desde un celular android

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { InicioPage } from './inicio/inicio.page';
 
 const routes: Routes = [
   {
